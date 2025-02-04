5596b3dce7f2eeac18bfc9008db4389fc453a4fc
FND <fnd@localhost.localdomain>
2025-02-04 08:00:00 +0100

    simplified file handling
    we don't actually have a streaming parser (that would be
    overengineering), so we need to read the entire file anyway

--- src/index.js
+++ src/index.js
@@ -1,51 +1,19 @@
 import { parseHeaders } from "./core.js";
-import { createReadStream } from "node:fs";
+import { readFile } from "node:fs/promises";
 
-let PROMISE = Promise.withResolvers ? Promise : { withResolvers };
-
-export function colonParse(filepath, options) {
-	let stream = createReadStream(filepath);
-	let { promise, resolve, reject } = PROMISE.withResolvers();
-	let data;
-	stream.on("data", (chunk) => {
-		if (data === undefined) {
-			data = chunk;
-			return;
+export async function colonParse(filepath, options) {
+	let data = await readFile(filepath);
+	let headers = new Map();
+	let decoder = new TextDecoder();
+	let key = null;
+	for (let item of parseHeaders(data, options?.trim === true)) {
+		item = decoder.decode(item);
+		if (key === null) {
+			key = item;
+		} else {
+			headers.set(key, item);
+			key = null;
 		}
-
-		let offset = data.length;
-		let buf = new Uint8Array(offset + chunk.length);
-		buf.set(data);
-		buf.set(chunk, offset);
-		data = buf;
-	});
-	stream.on("error", (err) => {
-		reject(err);
-	});
-	stream.on("end", () => {
-		let headers = new Map();
-		let decoder = new TextDecoder();
-		let key = null;
-		for (let item of parseHeaders(data, options?.trim === true)) {
-			item = decoder.decode(item);
-			if (key === null) {
-				key = item;
-			} else {
-				headers.set(key, item);
-				key = null;
-			}
-		}
-		let body = key;
-		resolve({ headers, body });
-	});
-	return promise;
-}
-
-function withResolvers() {
-	let res = {};
-	res.promise = new Promise((resolve, reject) => {
-		res.resolve = resolve;
-		res.reject = reject;
-	});
-	return res;
+	}
+	return { headers, body: key };
 }
